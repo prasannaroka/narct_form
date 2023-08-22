86d73e4e10f8a4c68145cbc597de74d84aa543a5
prasanna roka <prasannaroka600@gmail.com>
2023-08-22 18:07:10 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: { 
+
+      // fontFamily: {
+      //   'custom': ['Poppins', sans-serif], 
+      // },
+    },
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       screens: {
+//         'custom': {'min': '1081px', 'max': '1362px'},
+//       },
+//     },
+//   },
+//   variants: {},
+//   plugins: [],
+// };
